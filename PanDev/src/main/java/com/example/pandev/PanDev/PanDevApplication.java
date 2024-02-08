b03be52ff5d0fde4e00e98903847b6700a2e9145
alisher <alisher.sharipov2@Mail.ru>
2024-02-09 05:29:30 +0600

    init push

---
+++ PanDev/src/main/java/com/example/pandev/PanDev/PanDevApplication.java
@@ -0,0 +1,13 @@
+package com.example.pandev.PanDev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PanDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PanDevApplication.class, args);
+	}
+
+}
---
+++ PanDev/src/test/java/com/example/pandev/PanDev/PanDevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pandev.PanDev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PanDevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
